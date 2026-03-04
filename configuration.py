1eb0176ec898d241ff506d61cdf26c631067b6c5
Omar Hernández Yebra <omar.ye.omar@gmail.com>
2026-03-04 17:18:59 -0600

    Mi primer commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://cnt-3a2cda53-8cdf-47f4-92aa-3be0b2517d2c.containerhub.tripleten-services.com"
+DOC_PATH = "/docs/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
+
